05a52b9e5e6466af090cee30efb04665730a1232
Galo Navarro <anglorvaroa@gmail.com>
2019-04-21 22:30:31 +0200

    Sample
    Signed-off-by: Galo Navarro <anglorvaroa@gmail.com>

---
+++ jsonrouter/go.mod
@@ -0,0 +1,5 @@
+module github.com/srvaroa/faas/jsonrouter
+
+go 1.12
+
+require github.com/srvaroa/jsonrouter v0.0.0-20190605203932-047d99d01257 // indirect
---
+++ jsonrouter/handler.go
@@ -0,0 +1,40 @@
+package function
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/srvaroa/jsonrouter/pkg/forwarders"
+	"github.com/srvaroa/jsonrouter/pkg/router"
+)
+
+func Handle(req []byte) string {
+
+	configData := os.Getenv("config")
+	if len(configData) == 0 {
+		return "Could not read route configuration (or empty)"
+		os.Exit(1)
+	}
+
+	configDataBytes := []byte(configData)
+	routes, err := router.NewRoutingTable(&configDataBytes)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	json_raw := string(req)
+	res, err := routes.FindMatches(&json_raw)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	dump := forwarder.HttpForwarder{}
+	for endpoint, _ := range res {
+		err = dump.Send(&req, endpoint)
+		if err != nil {
+			return fmt.Sprintf("Failed to forward event", err)
+		}
+	}
+
+	return "OK"
+}
